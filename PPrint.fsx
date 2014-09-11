73f3a04416131135932832d0077daa594f41e403
Vesa Karvonen <vesa.a.j.k@gmail.com>
2014-09-11 19:05:32 +0300

    Mono friendly paths.

--- PPrint.fsx
+++ PPrint.fsx
@@ -1,9 +1,9 @@
 #I __SOURCE_DIRECTORY__ ;;
 
-#r "Libs\\PPrint.Pretty\\bin\\Release\\PPrint.dll" ;;
-#r "Libs\\PPrint.Pretty\\bin\\Release\\Infers.dll" ;;
-#r "Libs\\PPrint.Pretty\\bin\\Release\\Infers.Rep.dll" ;;
-#r "Libs\\PPrint.Pretty\\bin\\Release\\PPrint.Pretty.dll" ;;
+#r "Libs/PPrint.Pretty/bin/Release/PPrint.dll" ;;
+#r "Libs/PPrint.Pretty/bin/Release/Infers.dll" ;;
+#r "Libs/PPrint.Pretty/bin/Release/Infers.Rep.dll" ;;
+#r "Libs/PPrint.Pretty/bin/Release/PPrint.Pretty.dll" ;;
 
 open System ;;
 open PPrint ;;
