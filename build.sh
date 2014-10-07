7b58784c2e6be6debfa33035d3e85da635b7a782
Vesa Karvonen <vesa.karvonen@reaktor.fi>
2014-10-07 09:11:24 +0300

    Silence.

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-set -x
 set -e
 
-nuget restore PPrint.sln
+nuget restore PPrint.sln -Verbosity quiet
 
-xbuild /p:Configuration=Debug
-mono Tests/Examples/bin/Debug/Examples.exe
+function build () {
+    xbuild /nologo /verbosity:quiet /p:Configuration=$2 $1
+    mono Tests/Examples/bin/$2/Examples.exe
+}
 
-xbuild /p:Configuration=Release
-mono Tests/Examples/bin/Release/Examples.exe
+build PPrint.sln Debug
+build PPrint.sln Release
