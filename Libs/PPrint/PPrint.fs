8a4b67356e6bd62b04ebd0641eaf8444b708bbba
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-01-01 14:37:23 +0200

    Minor tweaks.

--- Libs/PPrint/PPrint.fs
+++ Libs/PPrint/PPrint.fs
@@ -6,34 +6,33 @@ open System
 open System.IO
 
 type Doc =
-  | EMPTY
-  | LAZY of Lazy<Doc>
-  | LINE of bool
-  | JOIN of lhs: Doc * rhs: Doc
-  | NEST of string * Doc
-  | TEXT of string
-  | CHOICE of wide: Doc * narrow: Doc
-  | COLUMN of (int -> Doc)
-  | NESTING of (int -> Doc)
-  | USER of obj
+  | Empty
+  | Lazy of Lazy<Doc>
+  | Line of Doc
+  | Join of lhs: Doc * rhs: Doc
+  | Nest of string * Doc
+  | Text of string
+  | Choice of wide: Doc * narrow: Doc
+  | With of (int -> string -> Doc)
+  | User of obj
 
 [<AutoOpen>]
 module Consts =
-  let space = TEXT " "
-  let line = LINE false
-  let linebreak = LINE true
-  let softline = CHOICE (space, line)
-  let softbreak = CHOICE (EMPTY, linebreak)
+  let space = Text " "
+  let line = Line space
+  let linebreak = Line Empty
+  let softline = Choice (space, line)
+  let softbreak = Choice (Empty, linebreak)
 
-  let inline joinWith d l r = JOIN (l, JOIN (d, r))
+  let inline join3 l d r = Join (l, Join (d, r))
 
 type Doc with
-  static member (<^>) (l, r) = JOIN (l, r)
-  static member (<+>) (l, r) = joinWith space l r
-  static member (<.>) (l, r) = joinWith line l r
-  static member (</>) (l, r) = joinWith softline l r
-  static member (<..>) (l, r) = joinWith linebreak l r
-  static member (<//>) (l, r) = joinWith softbreak l r
+  static member (<^>)  (l, r) = Join (l, r)
+  static member (<+>)  (l, r) = join3 l space     r
+  static member (<.>)  (l, r) = join3 l line      r
+  static member (</>)  (l, r) = join3 l softline  r
+  static member (<..>) (l, r) = join3 l linebreak r
+  static member (<//>) (l, r) = join3 l softbreak r
 
 [<AutoOpen>]
 module Util =
@@ -44,85 +43,82 @@ module Util =
 
 [<AutoOpen>]
 module PPrint =
-  let delay th = LAZY ^ Lazy.Create th
+  let delay th = Lazy ^ Lazy.Create th
 
-  let empty = EMPTY
+  let empty = Empty
   let space = space
   let line = line
   let linebreak = linebreak
   let softline = softline
   let softbreak = softbreak
 
-  let column i2d = COLUMN i2d
-  let nesting i2d = NESTING i2d
+  let inline column' withCol = With ^ fun col _ -> withCol col
+  let inline nesting' withNest = With ^ fun _ prefix -> withNest prefix.Length
 
-  let txt s = TEXT s
-  let user (x: obj) = USER x
-  let fmt f = Printf.ksprintf TEXT f
-  let chr (c: char) = TEXT ^ string c
+  let column withCol = column' withCol
+  let nesting withNest = nesting' withNest
 
-  let lparen = TEXT "("
-  let rparen = TEXT ")"
-  let lrparen = (lparen, rparen)
-  let langle = TEXT "<"
-  let rangle = TEXT ">"
-  let lrangle = (langle, rangle)
-  let lbrace = TEXT "{"
-  let rbrace = TEXT "}"
-  let lrbrace = (lbrace, rbrace)
-  let lbracket = TEXT "["
-  let rbracket = TEXT "]"
-  let lrbracket = (lbracket, rbracket)
-  let squote = TEXT "'"
+  let txt s = Text s
+  let user (x: obj) = User x
+  let fmt f = Printf.ksprintf Text f
+  let chr (c: char) = Text ^ string c
+
+  let (lparen,   rparen)   as lrparen   = (Text "(", Text ")")
+  let (langle,   rangle)   as lrangle   = (Text "<", Text ">")
+  let (lbrace,   rbrace)   as lrbrace   = (Text "{", Text "}")
+  let (lbracket, rbracket) as lrbracket = (Text "[", Text "]")
+  let squote = Text "'"
   let lrsquote = (squote, squote)
-  let dquote = TEXT "\""
+  let dquote = Text "\""
   let lrdquote = (dquote, dquote)
-  let semi = TEXT ";"
-  let colon = TEXT ":"
-  let comma = TEXT ","
-  let dot = TEXT "."
-  let backslash = TEXT "\\"
-  let equals = TEXT "="
+
+  let semi      = Text ";"
+  let colon     = Text ":"
+  let comma     = Text ","
+  let dot       = Text "."
+  let backslash = Text "\\"
+  let equals    = Text "="
 
   let punctuate sep (ds: seq<Doc>) = seq {
     use ds = ds.GetEnumerator ()
     if ds.MoveNext () then
-      let d = ref ds.Current
+      let mutable d = ds.Current
       while ds.MoveNext () do
-        yield !d <^> sep
-        d := ds.Current
-      yield !d
+        yield d <^> sep
+        d <- ds.Current
+      yield d
   }
 
-  let spaces n = String.replicate n " "
+  let inline spaces n = String.replicate n " "
 
-  let nestBy s d = NEST (s, d)
+  let nestBy s d = Nest (s, d)
   let nest n = nestBy ^ spaces n
 
-  let align d = column ^ fun k -> nesting ^ fun i -> d |> nest ^ k-i
+  let align d = With ^ fun c p -> d |> nest ^ c-p.Length
   let hang i d = align ^ nest i d
   let indent i d = txt ^ spaces i <^> d |> hang i
 
-  let width d f = column ^ fun l -> d <^> column ^ fun r -> f ^ r-l
+  let inline width' d f = column' ^ fun l -> d <^> column' ^ fun r -> f ^ r-l
+  let width d f = width' d f
 
-  let mkFill p t f d =
-    width d ^ fun w -> if p f w then t f else txt ^ spaces ^ f-w
+  let inline mkFill p t f d =
+    width' d ^ fun w -> if p f w then t f else txt ^ spaces ^ f-w
 
   let fillBreak n d = mkFill (<) (flip nest linebreak) n d
   let fill n d = mkFill (<=) (K empty) n d
 
   let rec flatten doc = delay ^ fun () ->
     match doc with
-     | LAZY doc -> flatten ^ force doc
-     | JOIN (lhs, rhs) -> JOIN (flatten lhs, flatten rhs)
-     | NEST (txt, doc) -> NEST (txt, flatten doc)
-     | EMPTY | TEXT _ | USER _ -> doc
-     | LINE b -> if b then empty else space
-     | CHOICE (wide, _) -> wide
-     | COLUMN f -> COLUMN (flatten << f)
-     | NESTING f -> NESTING (flatten << f)
-  let choice wide narrow = CHOICE (flatten wide, narrow)
-  let group doc = CHOICE (flatten doc, doc)
+     | Lazy doc -> flatten ^ force doc
+     | Join (lhs, rhs) -> Join (flatten lhs, flatten rhs)
+     | Nest (txt, doc) -> Nest (txt, flatten doc)
+     | Empty | Text _ | User _ -> doc
+     | Line wide -> wide
+     | Choice (wide, _) -> wide
+     | With f -> With <| fun c p -> f c p |> flatten
+
+  let choice wide narrow = Choice (flatten wide, narrow)
+  let group doc = choice doc doc
 
   let gnest n = group >> nest n
 
@@ -131,31 +127,34 @@ module PPrint =
      | [] -> empty
      | x::xs -> List.fold (flip bop) x xs
   let joinWith bop xs = joinWith' bop xs
-  let joinSep sep xs = xs |> joinWith' ^ fun l r -> l <^> (sep <^> r)
-  let hsep xs = joinWith' (<+>) xs
-  let hcat xs = joinWith' (<^>) xs
-  let vsep xs = joinWith' (<.>) xs
-  let vcat xs = joinWith' (<..>) xs
-  let fillSep xs = joinWith' (</>) xs
+
+  let joinSep sep xs = xs |> joinWith' ^ fun l r -> join3 l sep r
+
+  let hsep    xs = joinWith' (<+>)  xs
+  let hcat    xs = joinWith' (<^>)  xs
+  let vsep    xs = joinWith' (<.>)  xs
+  let vcat    xs = joinWith' (<..>) xs
+  let fillSep xs = joinWith' (</>)  xs
   let fillCat xs = joinWith' (<//>) xs
 
   let sep xs = group ^ vsep xs
   let cat xs = group ^ vcat xs
 
-  let inline enclose' (l: Doc, r: Doc) d = l <^> (d <^> r)
+  let inline enclose' (l, r) d = join3 l d r
   let enclose (l, r) d = enclose' (l, r) d
-  let squotes d = enclose' lrsquote d
-  let dquotes d = enclose' lrdquote d
-  let parens d = enclose' lrparen d
-  let angles d = enclose' lrangle d
-  let braces d = enclose' lrbrace d
+
+  let squotes  d = enclose' lrsquote  d
+  let dquotes  d = enclose' lrdquote  d
+  let parens   d = enclose' lrparen   d
+  let angles   d = enclose' lrangle   d
+  let braces   d = enclose' lrbrace   d
   let brackets d = enclose' lrbracket d
 
   type t =
-   | NIL
-   | PRINT of text: string * Lazy<t>
-   | LINEFEED of prefix: string * Lazy<t>
-   | OBJ of obj: obj * Lazy<t>
+   | Nil
+   | Print of text: string * Lazy<t>
+   | Linefeed of prefix: string * Lazy<t>
+   | Obj of obj: obj * Lazy<t>
 
   type [<AbstractClass>] Actions () =
     abstract Line: unit -> unit
@@ -166,60 +165,57 @@ module PPrint =
 
   let rec output (actions: Actions) doc =
     match force doc with
-     | NIL -> ()
-     | PRINT (str, doc) ->
+     | Nil -> ()
+     | Print (str, doc) ->
        actions.Write str
        output actions doc
-     | OBJ (obj, doc) ->
+     | Obj (obj, doc) ->
        actions.User obj
        output actions doc
-     | LINEFEED (prefix, doc) ->
+     | Linefeed (prefix, doc) ->
        actions.Line ()
        actions.Write prefix
        output actions doc
 
-  let rec fits maxColsOr0 usedCols doc =
-    maxColsOr0 = 0 ||
-    usedCols <= maxColsOr0 &&
+  let rec fits maxCols usedCols doc =
+    usedCols <= maxCols &&
     match force doc with
-     | NIL | LINEFEED _ -> true
-     | OBJ (_, doc) ->
-       fits maxColsOr0 usedCols doc
-     | PRINT (str, doc) ->
-       fits maxColsOr0 (usedCols + str.Length) doc
+     | Nil | Linefeed _ -> true
+     | Obj (_, doc) ->
+       fits maxCols usedCols doc
+     | Print (str, doc) ->
+       fits maxCols (usedCols + str.Length) doc
 
   type Docs =
     | Done
     | Docs of string * Doc * Docs
 
   let rec layout maxColsOr0 usedCols = function
-    | Done -> NIL
+    | Done -> Nil
     | Docs (prefix, doc, rest) ->
       match doc with
-       | LAZY doc ->
+       | Lazy doc ->
          layout maxColsOr0 usedCols ^ Docs (prefix, force doc, rest)
-       | EMPTY ->
+       | Empty ->
          layout maxColsOr0 usedCols rest
-       | JOIN (lhs, rhs) ->
+       | Join (lhs, rhs) ->
          layout maxColsOr0 usedCols ^
          Docs (prefix, lhs, Docs (prefix, rhs, rest))
-       | NEST (txt, doc) ->
+       | Nest (txt, doc) ->
          layout maxColsOr0 usedCols ^ Docs (prefix + txt, doc, rest)
-       | TEXT str ->
-         PRINT (str, lazy layout maxColsOr0 (usedCols + str.Length) rest)
-       | USER obj ->
-         OBJ (obj, lazy layout maxColsOr0 usedCols rest)
-       | LINE _ ->
-         LINEFEED (prefix, lazy layout maxColsOr0 prefix.Length rest)
-       | CHOICE (wide, narrow) ->
+       | Text str ->
+         Print (str, lazy layout maxColsOr0 (usedCols + str.Length) rest)
+       | User obj ->
+         Obj (obj, lazy layout maxColsOr0 usedCols rest)
+       | Line _ ->
+         Linefeed (prefix, lazy layout maxColsOr0 prefix.Length rest)
+       | Choice (wide, narrow) ->
          let wide = layout maxColsOr0 usedCols ^ Docs (prefix, wide, rest)
-         if fits maxColsOr0 usedCols ^ lazy wide
+         if maxColsOr0 = 0 || fits maxColsOr0 usedCols ^ lazy wide
          then wide
          else layout maxColsOr0 usedCols ^ Docs (prefix, narrow, rest)
-       | COLUMN f ->
-         layout maxColsOr0 usedCols ^ Docs (prefix, f usedCols, rest)
-       | NESTING f ->
-         layout maxColsOr0 usedCols ^ Docs (prefix, f prefix.Length, rest)
+       | With f ->
+         layout maxColsOr0 usedCols ^ Docs (prefix, f usedCols prefix, rest)
 
   let outputWithActions actions maxCols doc =
     let maxColsOr0 =
@@ -232,7 +228,7 @@ module PPrint =
   let inline outputWithFun write maxCols doc =
     outputWithActions
       {new Actions () with
-        member this.Write s = write s}
+        member t.Write s = write s}
       maxCols
       doc
 
